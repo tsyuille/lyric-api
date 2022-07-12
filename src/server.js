436bf60e6ee9db5ab5a856a5b3329858b4d46890
TaVon <tsyuille@gmail.com>
2022-07-12 10:09:59 -0400

    Added EJS functionality

--- src/server.js
+++ src/server.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const routes = require('./routes')
+const expressLayouts = require('express-ejs-layouts')
 const cors = require('cors')
 
 require('dotenv').config({path: __dirname + '/.env'})
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('public'))
+app.use(expressLayouts)
 app.use(routes)
+
+app.set('view engine', 'ejs')
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
